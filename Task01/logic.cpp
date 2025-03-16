b44e2d16ddecc01649a3a7ff3266aea62788e66f
linchic-l <lebedevskayalina@gmail.com>
2025-03-16 19:42:42 +0300

    Update project

--- Task01/logic.cpp
+++ Task01/logic.cpp
@@ -1,22 +1,21 @@
 #include "logic.h"
-#include <iostream>
-using namespace std;
 
 void bubble_sort(int* array, int size, bool type) {
-	int count = 0;
 	for (int j = 0; j < size - 1; j++)
 	{
-		for (int i = 0; i < size - 1; i++)
-		{
-			count++;
-			if ( type ? array[i] > array[i + 1] :array[i] < array[i + 1]) {
+		bool flag = true;
 
+		for (int i = 0; i < size - 1 - j; i++)
+		{
+			if ( type ? array[i] > array[i + 1] :array[i] < array[i + 1]) {
+				flag = false;
 				int a = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = a;
 			}
-			 
+		}
+		if (flag) {
+			break;
 		}
 	}
-	cout << "Count: " << count << endl;
 }
